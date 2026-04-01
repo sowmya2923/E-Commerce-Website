e5e5a3f5cfb9f1c1bd8a3d3a28f411162c797cbe
sowmya2923 <pothurajusowmya29gmail.com>
2026-04-01 16:50:09 +0530

    1

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ require("dotenv").config()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://e-commerce-website-1-xdi7.onrender.com"],
     credentials:true
 }))
 connectDB(app)
